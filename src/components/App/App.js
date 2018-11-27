c2fcecbcdd11e71f504f2e8834b1f9b04912c79f
nivas <srinivasa@Administrators-MacBook-Pro.local>
2018-11-27 01:01:57 -0800

    Fetch news from reddit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,21 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: {}
+    };
+  }
+
+  async componentDidMount() {
+    const response = await fetch("https://www.reddit.com/r/news.json");
+    const posts = await response.json();
+    this.setState({
+      posts: posts.data
+    });
+  }
+
   render() {
     return <div className="App">Hello world</div>;
   }
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,11 +1,29 @@
 import React from "react";
 import App from "./App";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import "../../enzyme-setup";
 
 describe("App", () => {
-  const wrapper = shallow(<App />);
+  const posts = { data: ["a", "b", "c"] };
+
+  beforeEach(() => {
+    fetch = jest.fn(() => {
+      return Promise.resolve({
+        json: function() {
+          return posts;
+        }
+      });
+    });
+  });
+
+  const wrapper = mount(<App />);
   it("Renders App", () => {
     expect(wrapper.exists()).toBe(true);
+  });
+
+  it("update the state with posts", async () => {
+    const wrapper = await mount(<App />);
+    await wrapper.instance().componentDidMount();
+    expect(wrapper.state("posts")).toBe(posts.data);
   });
 });
