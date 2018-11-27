500d7ef3b42b8979cb5382655f186832fb021939
nivas <srinivasa@Administrators-MacBook-Pro.local>
2018-11-27 03:51:03 -0800

    Add css for post

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,7 +1,35 @@
 import React, { Component } from "react";
+import "./post.css";
 
 export default class Post extends Component {
   render() {
-    return <li>{this.props.post.title}</li>;
+    const post = this.props.post;
+    const d = new Date(0);
+    d.setUTCSeconds(post.created_utc);
+    return (
+      <div className="post-container">
+        <div className="details">
+          <h1 className="title">
+            <a href={post.url}>{post.title}</a>
+          </h1>
+          <span className="author">Posted by: {post.author}</span>
+          <span className="posted_time">at&nbsp;{d.toLocaleString()}</span>
+        </div>
+
+        <div className="votes">
+          <span>
+            <i className="fa fa-hand-o-up" />
+            &nbsp;&nbsp;
+            <span className="up">{post.ups}</span>
+          </span>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <span>
+            <i className="fa fa-hand-o-down" />
+            &nbsp;&nbsp;
+            <span className="down">{post.downs}</span>
+          </span>
+        </div>
+      </div>
+    );
   }
 }
--- src/components/Post/Post.test.js
+++ src/components/Post/Post.test.js
@@ -4,8 +4,17 @@ import { shallow } from "enzyme";
 import "../../enzyme-setup";
 
 describe("Post", () => {
+  const post = {
+    title: "test",
+    author: "Author",
+    created_utc: 14532543,
+    ups: 10,
+    downs: 1000
+  };
   it("display title", () => {
-    const wrapper = shallow(<Post post={{ title: "test" }} />);
-    expect(wrapper.html()).toBe("<li>test</li>");
+    const wrapper = shallow(<Post post={post} />);
+    expect(wrapper.html()).toBe(
+      '<div class="post-container"><div class="details"><h1 class="title"><a>test</a></h1><span class="author">Posted by: Author</span><span class="posted_time">at 6/17/1970, 9:49:03 PM</span></div><div class="votes"><span><i class="fa fa-hand-o-up"></i>  <span class="up">10</span></span>        <span><i class="fa fa-hand-o-down"></i>  <span class="down">1000</span></span></div></div>'
+    );
   });
 });
