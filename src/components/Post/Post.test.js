e38752eae1d2112829ee4094075f5b58a65848a7
nivas <srinivasa@Administrators-MacBook-Pro.local>
2018-11-28 15:31:34 +0530

    Fix test

--- src/components/Post/Post.test.js
+++ src/components/Post/Post.test.js
@@ -14,7 +14,7 @@ describe("Post", () => {
   it("display title", () => {
     const wrapper = shallow(<Post post={post} />);
     expect(wrapper.html()).toBe(
-      '<div class="post-container"><div class="details"><h1 class="title"><a>test</a></h1><span class="author">Posted by: Author</span><span class="posted_time"> at 6/17/1970, 9:49:03 PM</span></div><div class="votes"><span><i class="fa fa-hand-o-up"></i>  <span class="up">10</span></span>        <span><i class="fa fa-hand-o-down"></i>  <span class="down">1000</span></span></div></div>'
+      '<div class="post-container"><div class="details"><h1 class="title"><a>test</a></h1><span class="author">Posted by: Author</span><span class="posted_time"> at 6/18/1970, 10:19:03 AM</span></div><div class="votes"><span><i class="fa fa-hand-o-up"></i>  <span class="up">10</span></span>        <span><i class="fa fa-hand-o-down"></i>  <span class="down">1000</span></span></div></div>'
     );
   });
 });
